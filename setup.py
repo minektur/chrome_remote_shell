11d3f9de286064296dcf3d742d5c7237d75b937d
Brandon Craig Rhodes <brandon@rhodesmill.org>
2010-02-24 20:25:57 -0500

    Started Python project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='chrome_remote_shell',
+    description='Client for talking to the Google Chrome remote shell port',
+    author='Brandon Craig Rhodes',
+    author_email='brandon@rhodesmill.org',
+    packages=['chrome_remote_shell'],
+    version='1.0',
+    )
