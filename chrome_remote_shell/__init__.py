34107f641298b9b22573d181107488d1a4058c7b
Fred Clift <fred@clift.org>
2014-09-17 22:44:46 -0600

    missed some files

--- chrome_remote_shell/__init__.py
+++ chrome_remote_shell/__init__.py
@@ -1,30 +1,54 @@
-"""Client for the Google Chrome browser's remote debugging api.
+""" Client for the Google Chrome browser's remote debugging api.
 
-#fixme - new example
->>> import chrome_remote_shell
->>> shell = chrome_remote_shell.open(port=9222)
->>> shell.request('DevToolsService', command='ping')
-{u'data': u'ok', u'command': u'ping', u'result': 0}
+    > a = Shell(host='localhost', port=92222)
 
+    a.tablist has a list of details on open tabs.
 
-As a convenience, the shell connection object offers a method that, by
-injecting JavaScript into the first tab, commands Chrome to open a URL
-in a new tab::
+    > a.connect(tab=index, updateTabs=True)
 
-    shell.open_url('http://www.aldaily.com/')
+    will connect a.soc to the webservice endpoint for tablist[index]'th
+    tab.  index is an integer, and updateTabs is True or False. Both tab
+    and updateTabs are optional, defaulting to 0 and True respectively.
+
+    At this point a.soc.send and a.soc.recv will synchronously write
+    commands and read responses.  The api is semi-asynchronous with
+    responses for commands, but also spontaeneous events will be
+    send by the browser. For this kind of advance usage, select/pol
+    on soc is advised.
+
+    As a convenience, the shell connection object offers a method that, by
+    injecting JavaScript into the first tab, commands Chrome to open a URL
+    in a new tab::
+
+    a.open_url('http://www.aldaily.com/')
+
+    You can also optionally specify a different tab to operate on.
 """
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
+import json
 import requests
 import websocket
 
 class Shell(object):
-    """A remote debugging connection to Google Chrome."""
+    """A remote debugging connection to Google Chrome.
+
+       > a = Shell(host='localhost', port=92222)
+
+       a.tablist has a list of details on open tabs.
+
+       > a.connect(tab=index, update_tabs=True)
+
+       will connect a.soc to the webservice endpoint for tablist[index]'th
+       tab.  index is an integer, and update_tabs is True or False. Both tab
+       and updateTabs are optional, defaulting to 0 and True respectively.
+
+       At this point a.soc.send and a.soc.recv will synchronously write
+       commands and read responses.  The api is semi-asynchronous with
+       responses for commands, but also spontaeneous events will be
+       send by the browser. For this kind of advance usage, select/pol
+       on soc is advised.  """
 
     def __init__(self, host='localhost', port=9222):
+        """ init """
         self.host = host
         self.port = port
         self.soc = None
@@ -32,9 +56,14 @@ class Shell(object):
         self.find_tabs()
 
 
-    def connect(self, tab=None):
-        if not self.tablist:
-            find_tabs()
+    def connect(self, tab=None, update_tabs=True):
+        """Open a websocket connection to remote browser, determined by
+           self.host and self.port.  Each tab has it's own websocket
+           endpoint - you specify which with the tab parameter, defaulting
+           to 0.  The parameter update_tabs, if True, will force a rescan
+           of open tabs before connection. """
+        if update_tabs or not self.tablist:
+            self.find_tabs()
         if not tab:
             tab = 0
         wsurl = self.tablist[tab]['webSocketDebuggerUrl']
@@ -43,13 +72,15 @@ class Shell(object):
 
 
     def close(self):
+        """ Close websocket connection to remote browser."""
         if self.soc:
             self.soc.close()
+            self.soc = None
 
 
     def find_tabs(self):
         """Connect to host:port and request list of tabs
-             return list of dicts"""
+             return list of dicts of data about open tabs."""
         # find websocket endpoint
         response = requests.get("http://%s:%s/json" % (self.host, self.port))
         self.tablist  = json.loads(response.text)
@@ -57,12 +88,17 @@ class Shell(object):
 
 
     def open_url(self, url):
-        """Open a URL in the default tab."""
+        """Open a URL in the oldest tab."""
         if not self.soc:
             self.connect(tab=0)
+        #force the 'oldest' tab to load url
         navcom = json.dumps({"id":0,
                               "method":"Page.navigate",
                               "params":{"url":url}})
+        # This code would open a new window, but browsers really dont
+        # like doing so.  And, the results are irritating at best.
+        # navcom=json.dumps({"id":0,"method":"Runtime.evaluate",
+        #                    "params":{"expression": "window.open('%s', '_blank', 'toolbar=1,scrollbars=1,location=1,statusbar=0,menubar=1,resizable=1')" % (url) }})
         self.soc.send(navcom)
         return self.soc.recv()
 
--- setup.py
+++ setup.py
@@ -3,23 +3,27 @@ import sys
 from distutils.core import setup
 
 requirements = []
-if sys.version_info < (2, 6):
-    requirements.append('simplejson')
 
 setup(
     name='chrome_remote_shell',
     version='1.2',
     description='Client for talking to the Google Chrome remote shell port',
     long_description=chrome_remote_shell.__doc__.split('\n\n', 1)[1],
-    author='Brandon Craig Rhodes',
-    author_email='brandon@rhodesmill.org',
-    url='http://bitbucket.org/brandon/chrome_remote_shell/',
+    author='Fred Clift and Brandon Craig Rhodes',
+    author_email='fred@clift.org',
+    url='https://github.com/minektur/chrome_remote_shell',
     packages=['chrome_remote_shell'],
+    platforms='any',
+    license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
         'Topic :: Internet :: WWW/HTTP :: Browsers',
         ],
-    install_requires=requirements,
+    install_requires=['websocket-client'],
     )
