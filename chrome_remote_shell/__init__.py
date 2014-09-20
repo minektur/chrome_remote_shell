c276b50452803fa5febc181c9a9ad29b0858d20c
Fred Clift <fred@clift.org>
2014-09-20 11:17:35 -0600

    add some extra state detection

--- chrome_remote_shell/__init__.py
+++ chrome_remote_shell/__init__.py
@@ -67,6 +67,8 @@ class Shell(object):
         if not tab:
             tab = 0
         wsurl = self.tablist[tab]['webSocketDebuggerUrl']
+        if self.soc.connected:
+            self.soc.close()
         self.soc = websocket.create_connection(wsurl)
         return self.soc
 
@@ -86,7 +88,7 @@ class Shell(object):
 
     def open_url(self, url):
         """Open a URL in the oldest tab."""
-        if not self.soc:
+        if not self.soc or not.self.soc.connected:
             self.connect(tab=0)
         # force the 'oldest' tab to load url
         navcom = json.dumps({"id": 0,
